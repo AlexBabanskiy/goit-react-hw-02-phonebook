b5c20c7283d92a7efa744091462cf00ee190e7b5
AlexBabanskiy <124041034+AlexBabanskiy@users.noreply.github.com>
2023-08-10 14:55:45 +0200

    Phonebook v2

---
+++ src/components/Phonebook/DeleteButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DeleteButton = ({ onClick }) => (
+  <button onClick={onClick} type="button">
+    Delete
+  </button>
+);
+
+export default DeleteButton;
---
+++ src/components/Phonebook/Phonebook.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const PhonebookContainer = styled.div`
+  background-color: #fff;
+  border: 1px solid #ccc;
+  padding: 20px;
+  max-width: 400px;
+  margin: 20px auto;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Button = styled.button`
+  max-width: 100px;
+`;
+
+export const H3 = styled.h3`
+  margin-bottom: 5px;
+`;
